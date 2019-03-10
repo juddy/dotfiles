4ac2de42462bc35e5df0d675929278829ad86f10
Your Name <you@example.com>
2019-03-10 10:45:52 -0700

    setup_debian_box.sh: apt-transport-https, cryptsetup as step 0

--- setup_debian_box.sh
+++ setup_debian_box.sh
@@ -3,6 +3,9 @@
 # 'i swear i have one of these already' edition
 # Sun Mar 10 10:06:28 PDT 2019 
 
+#Step zero
+sudo apt-get install apt-transport-https cryptsetup git 
+
 #Add atlassian-sdk apt repo details
 	sudo sh -c 'echo "deb https://packages.atlassian.com/atlassian-sdk-deb stable contrib" >>/etc/apt/sources.list'
 	wget https://packages.atlassian.com/api/gpg/key/public    
@@ -16,7 +19,7 @@
 
 #Install packages
 	sudo apt-get update
-	sudo apt-get -y install git bash spotify virtualbox qemu claws-mail xmonad wmaker scrot xdesktopwaves st atlassian-plugin-sdk spotify-client
+	sudo apt-get -y install spotify virtualbox qemu claws-mail xmonad wmaker scrot xdesktopwaves st atlassian-plugin-sdk spotify-client vagrant 
 
 #get dotfiles
 	cd ~
